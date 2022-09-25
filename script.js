840bbae1a167f32474d89982225dbfb230ee6a06
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-09-26 00:09:35 +0100

    The srcPOSTruns endpoint is now completed!

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let c = async () => {
 	app.use((req, res, next) => {
     	res.append('Access-Control-Allow-Origin', ['*']);
     	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    	res.append('Access-Control-Allow-Headers', 'X-API-key');
+    	res.append('Access-Control-Allow-Headers', 'X-API-key,Content-Type');
     	next();
 	});
 	app.post('/srcPOSTruns', (req, res) => {
@@ -22,22 +22,47 @@ let c = async () => {
 			res.json({status: 400, message: "Please submit an API key"})
 		} else if (req.body.length == 0) {
 			res.status = 400
-			res.json({status: 400, message: "Please submit a body with the desired run"})
+			res.json({status: 400, message: "Please submit an array with the desired run/runs"})
 		} else {
-			fetch("https://www.speedrun.com/api/v1/runs", {
-				method: "POST",
-				headers: {
-					"X-API-key": headers.get("x-api-key")
-				},
-				json: req.body
-			}).then(x => x.json()).then(r => {
-				if (!r.ok) {
-					res.status = r.status
-					res.json({status: r.status, message: r.message})
-				} else {
-					res.json({json: r})
+			console.log(typeof req.body)
+			if (Array.isArray(req.body)) {
+				let jsonobj = []
+				for (let run of req.body) {
+					fetch("https://www.speedrun.com/api/v1/runs", {
+						method: "POST",
+						headers: {
+							"X-API-key": headers.get("x-api-key"),
+							"Content-Type": "json"
+						},
+						body: JSON.stringify(run)
+					}).then(x => x.json()).then(r => {
+						if (!r.ok) {
+							jsonobj = jsonobj.concat([{status: r.status, message: r.message}])
+						} else {
+							jsonobj = jsonobj.concat([{json: r}])
+						}
+						if (jsonobj.length == req.body.length) {
+							res.json(jsonobj)
+						}
+					})
 				}
-			})
+			} else {
+				fetch("https://www.speedrun.com/api/v1/runs", {
+					method: "POST",
+					headers: {
+						"X-API-key": headers.get("x-api-key"),
+						"Content-Type": "json"
+					},
+					body:JSON.stringify(req.body)
+				}).then(x => x.json()).then(r => {
+					if (!r.ok) {
+						res.status = r.status
+						res.json({status: r.status, message: r.message})
+					} else {
+						res.json({json: r})
+					}
+				})
+			}
 		}
 	});
 }
